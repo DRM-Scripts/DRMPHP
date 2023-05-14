761d697b7b5811a09717fd7fc3e82a0f47277da0
DRM-Scripts <85300094+DRM-Scripts@users.noreply.github.com>
2023-05-14 11:24:24 +0100

    Update api.php

--- panel/api.php
+++ panel/api.php
@@ -24,7 +24,7 @@ switch($Action){
     if($ClientIP=="127.0.0.1" || $ClientIP=="::1" || $ClientIP==$_SERVER["SERVER_ADDR"] || $App->AllowedIP($ChanID, $ClientIP)){
       $M3u8Path = $App->GetConfig("M3UDownloadURL");
       $ChName=str_replace(" ", "_", $Channel["ChannelName"]);
-      $M3u8File = $M3u8Path."/".$ChName."/hls/index.m3u8";
+      $M3u8File = $M3u8Path."/".$ChName."/stream/index.m3u8";
       header("location: ".$M3u8File);
     }
     break;    
