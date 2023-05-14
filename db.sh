3967d21f499ba44cbcbcf02aa4a57b4b734b6892
DRM-Scripts <85300094+DRM-Scripts@users.noreply.github.com>
2023-05-14 11:23:04 +0100

    Update db.sh

--- db.sh
+++ db.sh
@@ -9,10 +9,10 @@ if [ -f /root/.my.cnf ]; then
 	echo "Enter utf8 if you don't know what you are doing"
 	read charset
 	echo "Creating new MySQL database..."
-	mysql -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
+	sudo mysql -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
 	echo "Database successfully created!"
 	echo "Showing existing databases..."
-	mysql -e "show databases;"
+	sudo mysql -e "show databases;"
 	echo ""
 	echo "Please enter the NAME of the new MySQL database user! (example: user1)"
 	read username
@@ -20,15 +20,15 @@ if [ -f /root/.my.cnf ]; then
 	echo "Note: password will be hidden when typing"
 	read -s userpass
 	echo "Creating new user..."
-	mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
+	sudo mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
 	echo "User successfully created!"
 	echo ""
 	echo "Granting ALL privileges on ${dbname} to ${username}!"
-	mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
-	mysql -e "FLUSH PRIVILEGES;"
-	mysql ${dbname} < db.sql
-    mysql SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';
-    mysql SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION';
+	sudo mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
+	sudo mysql -e "FLUSH PRIVILEGES;"
+	sudo mysql ${dbname} < db.sql
+    	sudo mysql SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';
+    	sudo mysql SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION';
 	echo "You're good now :)"
 	exit
 	
@@ -43,10 +43,10 @@ else
 	echo "Enter utf8 if you don't know what you are doing"
 	read charset
 	echo "Creating new MySQL database..."
-	mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
+	sudo mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
 	echo "Database successfully created!"
 	echo "Showing existing databases..."
-	mysql -uroot -p${rootpasswd} -e "show databases;"
+	sudo mysql -uroot -p${rootpasswd} -e "show databases;"
 	echo ""
 	echo "Please enter the NAME of the new MySQL database user! (example: user1)"
 	read username
@@ -54,15 +54,15 @@ else
 	echo "Note: password will be hidden when typing"
 	read -s userpass
 	echo "Creating new user..."
-	mysql -uroot -p${rootpasswd} -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
+	sudo mysql -uroot -p${rootpasswd} -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
 	echo "User successfully created!"
 	echo ""
 	echo "Granting ALL privileges on ${dbname} to ${username}!"
-	mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
-	mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
-	mysql -uroot -p${rootpasswd} ${dbname} < db.sql
-    mysql -uroot -p${rootpasswd} SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';
-    mysql -uroot -p${rootpasswd} SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION';
+	sudo mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
+	sudo mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+	sudo mysql -uroot -p${rootpasswd} ${dbname} < db.sql
+    	sudo mysql -uroot -p${rootpasswd} SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';
+    	sudo mysql -uroot -p${rootpasswd} SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION';
 	echo "You're good now :)"
 	exit
 fi
