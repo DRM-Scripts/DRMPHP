b47261fa5651ccfe4f6689576f5cb96eace3bc04
Devata <devatadev@pm.me>
2023-05-19 21:11:38 +0800

    Update add.php

--- panel/add.php
+++ panel/add.php
@@ -345,7 +345,7 @@ if(isset($_POST["Save"]) && $_POST["Save"]==1){
 
       function addKey() {
         var childCount = $('#keys').children().length;
-        if (childCount >= 4) {
+        if (childCount >= 6) {
           alert('Maximum 4 keys allowed');
           return;
         }
