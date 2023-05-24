6a8244b689994cda1e324c2853a3dad5e8c17adf
DRM-Scripts <85300094+DRM-Scripts@users.noreply.github.com>
2023-05-24 21:51:36 +0100

    Update _htmlhead.php

--- panel/_htmlhead.php
+++ panel/_htmlhead.php
@@ -1,9 +1,9 @@
 <head>
   <meta charset="utf-8" />
-  <title>Dash Admin</title>
+  <title>DRM PHP</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-  <meta content="Themesbrand" name="author" />
+  <meta content="DRMPHP | MPD Panel" name="description" />
+  <meta content="DRMScripts Community" name="author" />
   <link rel="shortcut icon" href="assets/images/favicon.ico">
   <link href="assets/css/bootstrap-dark.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
   <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
