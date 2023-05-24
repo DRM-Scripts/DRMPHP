a017c8309332f0e8b837374f57b7cf5ca0295fc2
DRM-Scripts <85300094+DRM-Scripts@users.noreply.github.com>
2023-05-24 21:45:12 +0100

    Update _header.php

--- panel/_header.php
+++ panel/_header.php
@@ -11,7 +11,7 @@
         </span>
       </a>
 
-      <a href="index.html" class="logo logo-light">
+      <a href="index.php" class="logo logo-light">
         <span class="logo-sm">
           <img src="assets/images/logo.png" alt="" height="22">
         </span>
