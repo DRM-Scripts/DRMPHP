bf8f032536999544226464d20a404a29c783f9b8
DRM-Scripts <85300094+DRM-Scripts@users.noreply.github.com>
2023-06-02 21:17:18 +0100

    Update _footer.php

--- panel/_footer.php
+++ panel/_footer.php
@@ -1,8 +1,12 @@
+<?php
+$version = "v0.1b";
+?>
+
 <footer class="footer">
   <div class="container-fluid">
     <div class="row">
       <div class="col-sm-6">
-        <script>document.write(new Date().getFullYear())</script> © DRMPHP
+        <script>document.write(new Date().getFullYear())</script> Â© DRMPHP <?php echo $version; ?>v.
       </div>
     </div>
   </div>
