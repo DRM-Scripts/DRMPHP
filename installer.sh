8ec28c3703cafafb9fb05390f9efeed407bbf171
Dmytro <dima3008maks@gmail.com>
2023-06-16 15:05:33 +0300

    Remove sudo

--- installer.sh
+++ installer.sh
@@ -178,31 +178,31 @@ function installDRMPHP() {
 if [ -f /root/.my.cnf ]; then
 	echo "Please enter the NAME of the new MySQL database! (example: database1)"
 	read dbname
-	sudo sed -i "s/drm/$dbname/g" /var/www/html/_db.php
+	sed -i "s/drm/$dbname/g" /var/www/html/_db.php
 	echo "Please enter the MySQL database CHARACTER SET! (example: latin1, utf8, ...)"
 	echo "Enter utf8 if you don't know what you are doing"
 	read charset
 	echo "Creating new MySQL database..."
-	sudo mysql -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
+	mysql -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
 	echo "Database successfully created!"
 	echo "Showing existing databases..."
-	sudo mysql -e "show databases;"
+	mysql -e "show databases;"
 	echo ""
 	echo "Please enter the NAME of the new MySQL database user! (example: user1)"
-	read username
-	sudo sed -i "s/admin/$username/g" /var/www/html/_db.php
+    read username
+	sed -i "s/admin/$username/g" /var/www/html/_db.php
 	echo "Please enter the PASSWORD for the new MySQL database user!"
 	echo "Note: password will be hidden when typing"
 	read -s userpass
-	sudo sed -i "s/passwd/$userpass/g" /var/www/html/_db.php
+	sed -i "s/passwd/$userpass/g" /var/www/html/_db.php
 	echo "Creating new user..."
-	sudo mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
+	mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
 	echo "User successfully created!"
 	echo ""
 	echo "Granting ALL privileges on ${dbname} to ${username}!"
-	sudo mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
-	sudo mysql -e "FLUSH PRIVILEGES;"
-	sudo mysql ${dbname} < db.sql
+	mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
+	mysql -e "FLUSH PRIVILEGES;"
+	mysql ${dbname} < db.sql
 	echo "Please enter root user MySQL password!"
 	echo "Note: password will be hidden when typing"
 	read -s rootpasswd
@@ -224,31 +224,31 @@ else
 	read -s rootpasswd
 	echo "Please enter the NAME of the new MySQL database! (example: database1)"
 	read dbname
-	sudo sed -i "s/drm/$dbname/g" /var/www/html/_db.php
+	sed -i "s/drm/$dbname/g" /var/www/html/_db.php
 	echo "Please enter the MySQL database CHARACTER SET! (example: latin1, utf8, ...)"
 	echo "Enter utf8 if you don't know what you are doing"
 	read charset
 	echo "Creating new MySQL database..."
-	sudo mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
+	mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
 	echo "Database successfully created!"
 	echo "Showing existing databases..."
-	sudo mysql -uroot -p${rootpasswd} -e "show databases;"
+	mysql -uroot -p${rootpasswd} -e "show databases;"
 	echo ""
 	echo "Please enter the NAME of the new MySQL database user! (example: user1)"
 	read username
-	sudo sed -i "s/admin/$username/g" /var/www/html/_db.php
+	sed -i "s/admin/$username/g" /var/www/html/_db.php
 	echo "Please enter the PASSWORD for the new MySQL database user!"
 	echo "Note: password will be hidden when typing"
 	read -s userpass
-	sudo sed -i "s/passwd/$userpass/g" /var/www/html/_db.php
+	sed -i "s/passwd/$userpass/g" /var/www/html/_db.php
 	echo "Creating new user..."
-	sudo mysql -uroot -p${rootpasswd} -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
+	mysql -uroot -p${rootpasswd} -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
 	echo "User successfully created!"
 	echo ""
 	echo "Granting ALL privileges on ${dbname} to ${username}!"
-	sudo mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
-	sudo mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
-	sudo mysql -uroot -p${rootpasswd} ${dbname} < db.sql
+	mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
+	mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+	mysql -uroot -p${rootpasswd} ${dbname} < db.sql
 	# MySQL commands
     commands=$(cat <<EOF
     USE $dbname;
