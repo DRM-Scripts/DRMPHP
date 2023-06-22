7719162e1546110d47a479edd44af4d89b39792b
Dmytro <68398868+Higher08@users.noreply.github.com>
2023-06-23 00:00:49 +0300

    More fixes

--- installer.sh
+++ installer.sh
@@ -143,14 +143,14 @@ function installDRMPHP() {
   sed -i -r 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.4/apache2/php.ini
   echo " PHP configured successfully!"
 
-  read -p ' Change TorrServer web-port? (Yes/No) ' answer_cp </dev/tty
+  read -p ' Change DRMPHP web-port? (Yes/No) ' answer_cp </dev/tty
   if [ "$answer_cp" != "${answer_cp#[Yy]}" ]; then
     read -p ' Enter port number: ' answer_port </dev/tty
     if [[ $OS == "debian" ]] || [[ $OS == "ubuntu" ]]; then 
-      sed 's/80/${answer_port}/' /etc/apache2/ports.conf
-      sed 's/80/${answer_port}/' /etc/apache2/sites-enabled/000-default.conf
+      sed "s/80/${answer_port}/" /etc/apache2/ports.conf
+      sed "s/80/${answer_port}/" /etc/apache2/sites-enabled/000-default.conf
     else
-      sed 's/80/${answer_port}/' /etc/httpd/conf/httpd.conf
+      sed "s/80/${answer_port}/" /etc/httpd/conf/httpd.conf
     fi
   fi
   echo " Configuring permissions..."
